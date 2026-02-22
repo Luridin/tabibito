5e2058df2705d91a527336d304f29b18a34adc21
Luridin <30283583+Luridin@users.noreply.github.com>
2026-02-22 19:00:14 +0530

    v rewrites v3

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "picsum.photos",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/tabibito",
+  assetPrefix: "/tabibito",
+
   images: {
     remotePatterns: [
       {
